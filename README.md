8ccd39c7deeb2069454afe792aa2305bb911392e
Josie5734 <armyjoe999@gmail.com>
2025-09-28 00:22:32 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### Bappy Birfday Billiam
+
+this is a browser extention made out of a joke where the first letter of every word is replaced with a B
---
+++ billiam.js
@@ -0,0 +1,66 @@
+/*
+    what need:
+        for each element on page
+            get an element
+            do the findandreplace
+            set the element
+*/
+
+/*
+    plans:
+        add a toggle switch in the top right to turn it on/off
+*/
+
+
+//main program
+var element = document.querySelectorAll("h1" && "h2" && "h3" && "p"); //get all elements with text in them
+for (var i = element.length; i--;) { //for each element
+    element[i].textContent = FindandReplace(element[i].textContent) //replace the words
+}
+
+
+
+//function that takes a string and replaces all the first letters with B, returns the new string
+function FindandReplace(input) {
+
+    let str = input.split(' '); //split the string into individual words
+    let tempword = ""; //tempval for putting the new "B" word in
+    let tempstring = ""; //create a new empty string for putting all the new "B" words in
+
+    for (let word of str) { //loop through each word
+
+        if (isNaN(word)) { //if word is not a number
+
+            if (["a","e","i","o","u"].includes(word[0].toLowerCase())) { //if the first letter is a vowel
+
+                if (word[0].toUpperCase() == word[0]) { //if letter was capitial 
+                    tempword = "B" + word; //add a B to the start of the word
+                } else { //else lowercase
+                tempword = "b" + word; //add a b to the start of the word
+                }
+
+            } else if (["ch","kn","ph","qu","sh","st","th","wh"].includes(word[0].substring(0,2).toLowerCase())) { //else if first 2 letters are a digraph or other 2 letter combo sound
+
+                if (word[0].toUpperCase() == word[0]) {
+                    tempword = "B" + word.substring(2,word.length); //replace first 2 letters of word with B
+                } else {
+                    tempword = "b" + word.substring(2,word.length); //replace first 2 letters of word with b
+                }
+
+            } else { //else just replace first letter
+
+                if (word[0].toUpperCase() == word[0]) {
+                    tempword = "B" + word.substring(1,word.length); //replace first letter with B
+                } else {
+                    tempword = "b" + word.substring(1,word.length); //replace first letter with b
+                }
+                
+            }
+
+            tempstring = tempstring + tempword + " " //add word and a space after it to the total string
+        }
+    }
+
+    return tempstring; //return the changed string
+
+}
