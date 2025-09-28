ccc0a76152e59b05e3539642091e212f04707ee8
Josie5734 <armyjoe999@gmail.com>
2025-09-28 01:11:54 +0100

    added the code to check through multiple types of elements, now covers headings and <p>

--- billiam.js
+++ billiam.js
@@ -1,24 +1,17 @@
 /*
-    what need:
-        for each element on page
-            get an element
-            do the findandreplace
-            set the element
-*/
-
-/*
     plans:
         add a toggle switch in the top right to turn it on/off
 */
 
 
 //main program
-var element = document.querySelectorAll("h1" && "h2" && "h3" && "p"); //get all elements with text in them
+
+let element = document.querySelectorAll("p, h1, h2, h3"); //get all elements to change 
 for (var i = element.length; i--;) { //for each element
     element[i].textContent = FindandReplace(element[i].textContent) //replace the words
 }
 
-
+//<span> and <a> could also be included but seem to break if they are represented by img rather than text, not sure how to exclude them if they are just an img 
 
 //function that takes a string and replaces all the first letters with B, returns the new string
 function FindandReplace(input) {
@@ -27,19 +20,19 @@ function FindandReplace(input) {
     let tempword = ""; //tempval for putting the new "B" word in
     let tempstring = ""; //create a new empty string for putting all the new "B" words in
 
-    for (let word of str) { //loop through each word
+    for (let word of str) { //loop through each word in the string
 
         if (isNaN(word)) { //if word is not a number
 
             if (["a","e","i","o","u"].includes(word[0].toLowerCase())) { //if the first letter is a vowel
 
                 if (word[0].toUpperCase() == word[0]) { //if letter was capitial 
-                    tempword = "B" + word; //add a B to the start of the word
+                    tempword = "B" + word.toLowerCase(); //add a B to the start of the word and make the first letter lowercase
                 } else { //else lowercase
                 tempword = "b" + word; //add a b to the start of the word
                 }
 
-            } else if (["ch","kn","ph","qu","sh","st","th","wh"].includes(word[0].substring(0,2).toLowerCase())) { //else if first 2 letters are a digraph or other 2 letter combo sound
+            } else if (["ch","kn","ph","qu","sh","st","th","wh"].includes(word.substring(0,2).toLowerCase())) { //else if first 2 letters are a digraph or other 2 letter combo sound
 
                 if (word[0].toUpperCase() == word[0]) {
                     tempword = "B" + word.substring(2,word.length); //replace first 2 letters of word with B
